3a92b7c6d487b95c527b309098c43ecd97b07394
Vishesh Sinha <visheshsinha59@gmail.com>
2026-04-04 12:39:38 +0530

    Add service worker registration in App component
    Register service worker for offline capabilities.

--- platfo/pages/_app.js
+++ platfo/pages/_app.js
@@ -1,5 +1,14 @@
-import "../styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then(() => console.log("Platfo SW registered ✅"))
+        .catch((err) => console.log("SW error:", err));
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
